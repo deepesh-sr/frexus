cae08f9dbd87beac07e7d6f5d91fa2ced43910e4
deepesh-sr <220714100078@cutm.ac.in>
2026-01-04 21:54:26 +0530

    add section 5

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
 import Section4 from './components/Section4'
+import Section5 from './components/Section5'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <Section2/>
     <Section3/>
     <Section4/>
+    <Section5/>
     </>
   )
 }
