610dad54f0acb94461983303160943e6edca8332
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-01-04 20:39:02 +0530

    add: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Nav/>
+    <Hero/>
     </>
   )
 }
